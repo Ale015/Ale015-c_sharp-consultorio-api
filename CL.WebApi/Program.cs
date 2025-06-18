dbe04f3c229cd4ae07d1bde1a909efd1af7c7299
junior <veadrigojunior@gmail.com>
2025-06-18 11:27:35 -0300

    setup full working

---
+++ CL.WebApi/Program.cs
@@ -0,0 +1,34 @@
+// Cria o builder da WebApplication para adicionar as dependências e configurar o pipeline de requisições.
+using CL.Data.Context;
+using FluentAssertions.Common;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Adição de serviços ao contêiner.
+// Adição de Controller I Endpoints I Swagger
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ClContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("db_consultorio")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Adição de serviços de injeção de dependência
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Rodar a aplicação
+app.Run();
