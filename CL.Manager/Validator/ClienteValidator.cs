008959cc920fdcb9aad9ecc2eca6c1478647aba0
junior <veadrigojunior@gmail.com>
2025-07-16 10:52:15 -0300

    Início Fluent Validation

---
+++ CL.Manager/Validator/ClienteValidator.cs
@@ -0,0 +1,30 @@
+﻿using CL.Core.Domain;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL.Manager.Validator
+{
+    public class ClienteValidator : AbstractValidator<Cliente>
+    {
+
+        public ClienteValidator()
+        {
+            
+            RuleFor(c => c.Nome).NotEmpty().WithMessage("O nome do cliente não pode ser vazio.")
+                .Length(2, 100).WithMessage("O nome do cliente deve ter entre 2 e 100 caracteres.");
+            RuleFor(c => c.Email).NotEmpty().WithMessage("O Email não pode ser vazio.");
+            RuleFor(c => c.Sexo).Must(s => s == "M" || s == "F").WithMessage("O sexo deve ser 'M' ou 'F'.");
+            RuleFor(c => c.Telefone).NotEmpty().WithMessage("O telefone não pode ser vazio.")
+                .Matches("[2-9][0-9]{10}").WithMessage("O telefone deve conter entre 10 e 15 dígitos.");
+            RuleFor(c => c.Documento).NotEmpty().WithMessage("O documento não pode ser vazio.");
+
+
+
+        }
+
+    }
+}
